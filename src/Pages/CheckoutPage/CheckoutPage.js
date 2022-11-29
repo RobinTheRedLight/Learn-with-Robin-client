c5942bb468c2dd665d0bb3c1d9d489fc1b3dbf01
RobinTheRedLight <robinmitra789@gmail.com>
2022-11-29 11:33:07 +0600

    Checkout Page Added

--- src/Pages/CheckoutPage/CheckoutPage.js
+++ src/Pages/CheckoutPage/CheckoutPage.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import toast, { Toaster } from 'react-hot-toast';
 import { useLoaderData } from 'react-router-dom';
 
 const CheckoutPage = () => {
     const details = useLoaderData();
-    console.log(details);
+    const notify = () => toast.success('Purchase Complete', { duration: 1000 });
     return (
-        <div>
-            <h1>CheckoutPage</h1>
-            <h2>{details.name}</h2>
+        <div >
+            <div className='m-5 p-5 mx-auto w-1/2 border border-sky-500 rounded'>
+                <h1 className='text-center  p-6 text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-300 to-red-300 '>Checkout for '{details.name}'</h1>
+                <div className='text-center p-3'>
+                    <button onClick={notify} className="btn btn-outline btn-success">Purchase</button>
+                    <Toaster />
+                </div>
+            </div>
         </div>
     );
 };
